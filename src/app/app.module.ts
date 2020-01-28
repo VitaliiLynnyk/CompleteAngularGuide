9fbe8c1a788667d26ff17dbd87c1d53fb02b168f
VitaliiLynnyk <linnik2101@gmail.com>
2020-01-28 21:04:02 +0100

    add recipes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./containers/header/header.component";
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './containers/header/header.component';
 import { RecipesComponent } from './containers/recipes/recipes.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/app/containers/recipes/recipes.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.html',
+  styleUrls: ['./recipes.scss']
+})
+export class RecipesComponent {}
