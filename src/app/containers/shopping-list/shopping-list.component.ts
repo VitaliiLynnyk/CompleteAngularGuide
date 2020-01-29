64da81ccc9e2096f28da226afc17420d7449397d
VitaliiLynnyk <linnik2101@gmail.com>
2020-01-29 21:02:42 +0100

    add shopping-list style

--- src/app/containers/shopping-list/shopping-list.component.ts
+++ src/app/containers/shopping-list/shopping-list.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Ingredient } from '../../models/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.html',
   styleUrls: ['./shopping-list.scss']
 })
-export class ShoppingListComponent {}
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 1),
+  ];
+}
--- src/app/containers/shopping-list/shopping-list.module.ts
+++ src/app/containers/shopping-list/shopping-list.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { ShoppingListComponent } from './shopping-list.component';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 
 
 @NgModule({
-  imports: [],
+  imports: [BrowserModule],
   exports: [
     ShoppingListComponent
   ],
