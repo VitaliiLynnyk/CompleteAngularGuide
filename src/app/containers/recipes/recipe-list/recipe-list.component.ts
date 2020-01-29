3c0e5733f02df705f704cbeb11b149d99ce48b50
VitaliiLynnyk <linnik2101@gmail.com>
2020-01-29 19:44:03 +0100

    add recipe-item style to recipe-list.

--- src/app/containers/recipes/recipe-list/recipe-list.component.ts
+++ src/app/containers/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../../../models/recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.html',
   styleUrls: ['./recipe-list.scss']
 })
-export class RecipeListComponent {}
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test',
+      'simple test',
+      'https://images.media-allrecipes.com/images/89754.jpg'
+    )
+  ];
+}
--- src/app/containers/recipes/recipe-list/recipe-list.module.ts
+++ src/app/containers/recipes/recipe-list/recipe-list.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { RecipeListComponent } from './recipe-list.component';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 
 @NgModule({
-  imports: [],
+  imports: [BrowserModule],
   exports: [
     RecipeListComponent
   ],
