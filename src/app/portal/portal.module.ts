0c78cf85aa95136bbb1e6d7969abcfed0bf33a6f
VitaliiLynnyk <linnik2101@gmail.com>
2020-02-05 21:08:36 +0100

    setting up routes {(} /recipes, /shopping-list }.

--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContainerModule } from '../containers/container.module';
+import { PortalRoutingModule } from './portal-routing.module';
+
 import { PortalComponent } from './portal.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ContainerModule
+    ContainerModule,
+    PortalRoutingModule
   ],
-  exports: [PortalComponent],
-  declarations: [PortalComponent]
+  exports: [ PortalComponent ],
+  declarations: [ PortalComponent ]
 })
-export class PortalPageModule {}
+export class PortalPageModule {
+}
