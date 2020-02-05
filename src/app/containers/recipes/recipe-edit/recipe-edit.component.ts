813b44913f826ac7b582382b942409ad333994da
VitaliiLynnyk <linnik2101@gmail.com>
2020-02-05 22:35:14 +0100

    Retrieving Route Parameters. Checking edit mode or new mode.

--- src/app/containers/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/containers/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.html',
-  styleUrls: [ './recipe-edit.scss']
+  styleUrls: [ './recipe-edit.scss' ]
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.subscribeToCheckMode();
   }
 
+  subscribeToCheckMode() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+    });
+  }
 }
