07ec10aa4fc5bb6d92b0d96bfc3f641cfda17fdd
VitaliiLynnyk <linnik2101@gmail.com>
2020-02-07 21:31:20 +0100

    sync html with form

--- src/app/containers/container.module.ts
+++ src/app/containers/container.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { DropdownDirective } from '../directives/dropdown.directive';
 
@@ -19,7 +19,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     FormsModule,
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports: [
     HeaderComponent,
--- src/app/containers/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/containers/recipes/recipe-edit/recipe-edit.component.ts
@@ -31,6 +31,10 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  submit() {
+    console.log(this.recipeForm);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImgPath = '';
