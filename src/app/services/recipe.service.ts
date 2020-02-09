7711053f03154fe30c76d75f89209d62402ea9cd
VitaliiLynnyk <linnik2101@gmail.com>
2020-02-09 22:30:36 +0100

    add fetchRecipes method to ret recipes from firebase

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -13,35 +13,23 @@ import { Ingredient } from '../models/ingredient.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ]
-    )
-  ];
+  private recipes: Recipe[] = [];
 
   constructor(
     private http: HttpClient,
     private shoppingListService: ShoppingListService
   ) {}
 
+  fetchRecipes() {
+    this.http.get('https://angulardevguide.firebaseio.com/recipes.json')
+        .subscribe((res: Recipe[]) => {
+          this.setRecipes(res);
+        });
+  }
+
   storeRecipes() {
     this.http.put('https://angulardevguide.firebaseio.com/recipes.json', this.recipes)
-        .subscribe((res: Response) => console.log(res));
+        .subscribe((res: Recipe[]) => console.log(res));
   }
 
   getRecipes(): Recipe[] {
@@ -54,6 +42,11 @@ export class RecipeService {
 
   onAddToShoppingList(ingredients: Ingredient[]): void {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   addRecipe(recipe: Recipe) {
