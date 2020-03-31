b96eb51d07cf5417104f3cf00f5c448c83a13914
VitaliiLynnyk <linnik2101@gmail.com>
2020-03-31 21:15:46 +0200

    add update ingredient action

--- src/app/containers/shopping-list/store/shopping-list.actions.ts
+++ src/app/containers/shopping-list/store/shopping-list.actions.ts
@@ -4,6 +4,7 @@ import { Ingredient } from '../../../models/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
 
 export class AddIngrediend implements Action {
   readonly type = ADD_INGREDIENT;
@@ -17,4 +18,10 @@ export class AddIngrediends implements Action {
   constructor(public payload: Ingredient[]) { }
 }
 
-export type ShoppingListActions = AddIngrediend | AddIngrediends;
+export class UpdateIngrediend implements Action {
+  readonly type = UPDATE_INGREDIENT;
+
+  constructor(public payload: { index: number, ingredient: Ingredient }) { }
+}
+
+export type ShoppingListActions = AddIngrediend | AddIngrediends | UpdateIngrediend;
