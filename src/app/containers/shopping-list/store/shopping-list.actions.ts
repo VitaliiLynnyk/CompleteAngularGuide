6120ff89c28bba83028af6c9fbdd5173cf41ceaa
VitaliiLynnyk <linnik2101@gmail.com>
2020-03-31 21:17:05 +0200

    add delete ingredient action

--- src/app/containers/shopping-list/store/shopping-list.actions.ts
+++ src/app/containers/shopping-list/store/shopping-list.actions.ts
@@ -5,6 +5,7 @@ import { Ingredient } from '../../../models/ingredient.model';
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngrediend implements Action {
   readonly type = ADD_INGREDIENT;
@@ -24,4 +25,10 @@ export class UpdateIngrediend implements Action {
   constructor(public payload: { index: number, ingredient: Ingredient }) { }
 }
 
-export type ShoppingListActions = AddIngrediend | AddIngrediends | UpdateIngrediend;
+export class DeleteIngrediend implements Action {
+  readonly type = DELETE_INGREDIENT;
+
+  constructor(public payload: number) { }
+}
+
+export type ShoppingListActions = AddIngrediend | AddIngrediends | UpdateIngrediend | DeleteIngrediend;
